48c13798cdb1bbb954ebe2df9fe10b1159187246
GEEGII <gazasolomon@gmail.com>
2024-08-01 21:40:01 +0100

    locked boxes finding key

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Interview task
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = set([0])  # Start with the first box unlocked
+    keys = set(boxes[0])  # Keys from the first box
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
