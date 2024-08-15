79301be42bad2182946de0c62009949405305f31
GEEGII <gazasolomon@gmail.com>
2024-08-15 23:37:22 +0100

    ups

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -58,7 +58,8 @@ def main():
     try:
         for line in sys.stdin:
             status, file_size = parse_line(line.strip())
-            if status is not None and isinstance(status, int) and file_size is not None:
+            if status is not None and isinstance(status, int
+                                                 ) and file_size is not None:
                 total_size += file_size
                 status_codes[status] += 1
                 line_count += 1
@@ -69,6 +70,7 @@ def main():
     except KeyboardInterrupt:
         print_stats(total_size, status_codes)
         raise
+
 
 if __name__ == "__main__":
     main()
