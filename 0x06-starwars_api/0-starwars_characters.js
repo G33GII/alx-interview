629b0f231fd4c0f652569043d47c55ec129b849c
GEEGII <gazasolomon@gmail.com>
2024-09-08 19:36:21 +0100

    chore: Refactor Star Wars character retrieval code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,22 +8,14 @@ const API_URL = 'https://swapi-api.alx-tools.com/api';
 
 // Check if the number of command line arguments is greater than 2
 if (process.argv.length > 2) {
-    const movieID = process.argv[2];
-
     // Make a request to the film resource for the specified film ID
-    request(`${API_URL}/films/${movieID}/`, (err, _, body) => {
+    request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
         // If an error occurred during the request, log the error
         if (err) {
-            return console.log(`Error: ${err}`);
+            console.log(err);
         }
-
-        // Try to parse the response body and handle potential errors
-        let charactersURL;
-        try {
-            charactersURL = JSON.parse(body).characters;
-        } catch (parseErr) {
-            return console.log(`Failed to parse JSON: ${parseErr}`);
-        }
+        // Get the characters URL from the film's response body
+        const charactersURL = JSON.parse(body).characters;
 
         // Create an array of Promises that resolve with the names of the characters
         const charactersName = charactersURL.map(
@@ -32,25 +24,16 @@ if (process.argv.length > 2) {
                 request(url, (promiseErr, __, charactersReqBody) => {
                     // If an error occurred during the request, reject the Promise with the error
                     if (promiseErr) {
-                        return reject(promiseErr);
+                        reject(promiseErr);
                     }
-
-                    // Try to parse the character's body to get the name
-                    try {
-                        resolve(JSON.parse(charactersReqBody).name);
-                    } catch (characterParseErr) {
-                        reject(`Failed to parse character data: ${characterParseErr}`);
-                    }
+                    // Resolve the Promise with the name of the character
+                    resolve(JSON.parse(charactersReqBody).name);
                 });
-            })
-        );
+            }));
 
         // Wait for all Promises to resolve and log the names of the characters, separated by new lines
         Promise.all(charactersName)
             .then(names => console.log(names.join('\n')))
-            .catch(allErr => console.log(`Error fetching characters: ${allErr}`));
+            .catch(allErr => console.log(allErr));
     });
-} else {
-    // If no movie ID is provided, display usage instructions
-    console.log('Usage: ./script.js <movie_id>');
 }
